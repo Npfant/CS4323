e62d70a96d8e2713ab28dd6d92e47112d1eb5a3f
DylanPalmese <dylan.palmese@okstate.edu>
2025-04-03 17:35:14 -0500

    Update Initialization.c

--- Initialization.c
+++ Initialization.c
@@ -11,22 +11,8 @@ int main()
   char trains[5][100];
 
   for(int i = 0; i < 5; i++){
-    char str1[100];
-    char str2[100];
-    fgets(str1,100,intersections_init);
-    fgets(str2,100,trains_init);
-    for(int j = 0; j < 100; j++){
-      if(str1[j] == ':'){
-        strncpy(intersections[i],str1+j,100-j);
-        break;
-      }
-    }
-    for(int j = 0; j < 100; j++){
-      if(str2[j] == ':'){
-        strncpy(trains[i],str1+j,100-j);
-        break;
-      }
-    }
-    printf("%s\t\t%s",intersections[i],trains[i]);
+    fgets(intersections[i],100,intersections_init);
+    fgets(trains[0],100,trains_init);
+    //printf("%s\t\t%s",intersections[i],trains[i]);
   }
 }
