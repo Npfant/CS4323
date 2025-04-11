d08897c26fc0f4f39a8047d83ae8c096ae8f305f
nolan-voss <91168393+nolan-voss@users.noreply.github.com>
2025-04-10 19:59:38 -0500

    Update forking.c

--- forking.c
+++ forking.c
@@ -1,57 +1,44 @@
+//Author Name: Nolan Voss
+//Email: nolan.voss@okstate.edu
+//Date: 04/10/2025
+//Program Description: Forking for the trains
+
 #include <stdio.h>  // for printf
 #include <stdlib.h> // for exit(1)
 #include <unistd.h> // for fork
 #include <pthread.h> // for mutex
 
 int main() {
+  //Total number of trains
   #define CHILDCOUNT 5
-  
+
   char* train_name = "";
-  
-  int totalProcesses = 0;
-  
-  //pthread_mutex_t [lock];
-  
-  //sem_t lock[5]
-  
-  //parent reads train.txt for information 
-  //initialization.c
-  
-  /*
-  FILE *intersections_init = fopen("intersections.txt","r");
-  FILE *trains_init = fopen("trains.txt","r");
 
-  char intersections[5][100];
-  char trains[5][100];
-
-  for(int i = 0; i < 5; i++){
-    fgets(intersections[i],100,intersections_init);
-    fgets(trains[0],100,trains_init);
-    //printf("%s\t\t%s",intersections[i],trains[i]);
-  }
-  */
+  //Test variable
+  //int totalProcesses = 0;
   
   const char* childNames[CHILDCOUNT] = {"Train1", "Train2", "Train3", "Train4", "Train5"};
   
   int trainPID = fork(); 
-  
+
+  //Creates the forking process for the amount of trains planned
   for (int i = 0; i < CHILDCOUNT; i++){
     totalProcesses ++;
-    
+
     if (trainPID < 0) { // fork failed; exit
       fprintf(stderr, "fork failed\n");
       exit(1);
     } else if (trainPID == 0) { //child (new process)
       train_name = childNames[i];
       printf("TrainName: %s\n", train_name);
+      //Once we merge this with the main code, this will run train_behavior function.
 
     } else { //parent goes down this path (main)
       int trainPID = fork(); 
-      //printf("Parent: %d PID = %d PPID = %d\n", trainPID, getpid(), getppid());
     }
   }
   
-  printf("totalProcesses: %d", totalProcesses);
+  //printf("totalProcesses: %d", totalProcesses);
 
   return 0;
 }
