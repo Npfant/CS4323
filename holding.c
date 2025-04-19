11d6e9cce4c971b51bb89848e9f3d4a55adf7507
Npfant <npfant@gmail.com>
2025-04-19 00:15:23 -0500

    Changed file struct

---
+++ holding.c
@@ -0,0 +1,23 @@
+#include "rat_new.c"
+
+void add_train_to_holding(Intersection* inter, const char* train_name) {    //changed by me
+    if (inter->num_holding < MAX_HOLDING) {        
+       strncpy(inter->holding_trains[inter->num_holding], train_name, MAX_NAME_LEN - 1);
+       inter->holding_trains[inter->num_holding][MAX_NAME_LEN - 1] = '\0';  // Ensure null termination
+       inter->num_holding++;
+   } else {
+       printf("ERROR: Holding capacity reached at %s\n", inter->name);
+   }
+}
+
+void remove_train_from_holding(Intersection* inter, const char* train_name) {
+   for (int i = 0; i < inter->num_holding; i++) {
+       if (strcmp(inter->holding_trains[i], train_name) == 0) {
+           for (int j = i; j < inter->num_holding - 1; j++) {
+               strcpy(inter->holding_trains[j], inter->holding_trains[j + 1]);
+           }
+           inter->num_holding--;
+           break;
+       }
+   }
+}
---
+++ preemption.c
@@ -0,0 +1,28 @@
+//Author Name: Nathan Fant
+//Email: nathan.fant@okstate.edu
+//Date: 04/11/2025
+//Program Description: Implementation of a preemption deadlock mitigation strategy
+
+#include "intersections.c"
+
+void preemption(int trainx, const char* train_name, int req_id, int res_id, int numInter, int numTrains, Intersection* intersections, char** trains, int* req, int* alloc){
+    for(int i = 0; i < numInter; i++){
+        if(*(alloc + trainx * numInter + i) == 1){
+            Intersection* inter = &intersections[i];
+            if (inter->type == MUTEX) {
+                pthread_mutex_unlock(&inter->mutex);
+            } else {
+                sem_post(&inter->semaphore);
+            }
+            *(alloc + trainx * numInter + i) = 0;
+            *(req + trainx * numInter + i) = 1;
+        }
+    }
+    for(int i = 0; i < numInter; i++){
+        if(*(req + trainx * numInter + i) == 1){
+            Intersection* inter = &intersections[i];
+            acquire_intersection(train_name, inter->name, req_id, res_id, (int*) req, (int*) alloc, numTrains, numInter, intersections, trains);    //train enter :D
+            release_intersection(train_name, inter->name, req_id, res_id, (int*) req, (int*) alloc, numTrains, numInter, intersections, trains);    //train leave :(
+        }
+    }
+}
